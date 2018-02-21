ca1cbe7d7007e47d4c062d691df26cf5fe4ba92a
Robert Luiz <Robert.tiweb@gmail.com>
2018-02-21 17:00:00 -0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,32 @@
 module.exports = {
-    "extends": "airbnb"
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true
+        }
+
+    },
+    "env": {
+        "browser": true,
+    },
+    "plugins": [
+        "react"
+    ],
+    "extends": ["webpack", "airbnb"],
+    "rules": {
+        "import/no-extraneous-dependencies": ["error", {
+            "devDependencies": true,
+            "optionalDependencies": false,
+            "peerDependencies": false
+        }]
+    },
+    "settings": {
+        "import/resolver": {
+            'alias': [
+                ['modules', `${__dirname}/node_modules`],
+                ['jquery', 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js'],
+                ['bootstrap', 'modules/admin-lte/bootstrap/js/bootstrap.js']
+            ]
+        }
+    },
 };
